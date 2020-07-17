82047e607edc61540120fa3fbe99f32edbc2bd64
JesusJimenez98 <jesusjimeneztapia456@gmail.com>
2020-07-16 22:15:59 -0400

    Create React project.

---
+++ web/src/App.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import logo from './logo.svg'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div
+                style={{
+                    paddingTop: '1.4em',
+                    textAlign: 'center',
+                    backgroundColor: '#282c34',
+                    fontSize: '1.2rem',
+                }}
+            >
+                <Link
+                    className='App-link'
+                    to='/'
+                    style={{ margin: '1.4rem' }}
+                    title='Ir a inicio'
+                >
+                    Inicio
+                </Link>
+                <Link
+                    className='App-link'
+                    to='/about'
+                    style={{ margin: '1.4rem' }}
+                    title='Ir a Sobre Nosotros'
+                >
+                    Sobre Nosotros
+                </Link>
+            </div>
+
+            <Switch>
+                <Route
+                    exact
+                    path='/'
+                    component={() => {
+                        return (
+                            <div className='App'>
+                                <header className='App-header'>
+                                    <img src={logo} className='App-logo' alt='logo' />
+                                    <p>
+                                        Edit <code>src/App.js</code> and save to reload.
+                                    </p>
+                                    <a
+                                        className='App-link'
+                                        href='https://reactjs.org'
+                                        target='_blank'
+                                        rel='noopener noreferrer'
+                                    >
+                                        Learn React
+                                    </a>
+                                </header>
+                            </div>
+                        )
+                    }}
+                />
+                <Route
+                    exact
+                    path='/about'
+                    component={() => {
+                        return (
+                            <div className='App-header'>
+                                <strong>Sobre Nosotros</strong>
+                            </div>
+                        )
+                    }}
+                />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default App
