da3178ba9c0a2b02191ce4020675760dba1d6d09
Jesús Jimenez <jesusjimeneztapia456@gmail.com>
2020-07-20 02:03:15 -0400

    Create environment variables for Firebase.

--- web/src/config/firebase.js
+++ web/src/config/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAQJtvTmf-G_mPh7NIspO1VO-rlzmV9ly4',
-    authDomain: 'dai-umss.firebaseapp.com',
-    databaseURL: 'https://dai-umss.firebaseio.com',
-    projectId: 'dai-umss',
-    storageBucket: 'dai-umss.appspot.com',
-    messagingSenderId: '388420815093',
-    appId: '1:388420815093:web:d3ce9f788e76ad3343ddbc',
-    measurementId: 'G-HN86WP037E',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 export default firebase.initializeApp(firebaseConfig)
